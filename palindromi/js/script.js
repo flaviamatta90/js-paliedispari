655d90fdb3017c0bac6a195af600393c4fedf42b
Flavia <flaviamatta90@gmail.com>
2020-08-27 16:22:14 +0200

    solution with reverse/spit/join

--- palindromi/js/script.js
+++ palindromi/js/script.js
@@ -1,1 +1,20 @@
+// 1 Chiedere all’utente di inserire una parola
+var parolaUtente = prompt("Inserisci una parola");
 
+
+// 2 Creare una funzione per capire se la parola inserita è palindroma
+var parola = palindroma(parolaUtente);
+console.log(parola);
+
+function palindroma() {
+  var scritta = parolaUtente.split("").reverse().join("");
+
+// se la parola scelta dall'utente è palindroma
+  if (parolaUtente == scritta) {
+    return parolaUtente + " è palindroma";
+  }
+// se la parola scelta dall'utente non è palindroma
+  else {
+    return parolaUtente + " non è palindroma";
+  }
+}
---
+++ paridispari/js/script.js
@@ -0,0 +1,11 @@
+// 1 L’utente sceglie pari o dispari
+//
+// 2 L’utente inserisce un numero da 1 a 5.
+//
+// 3 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+//
+// 4 Sommiamo i due numeri
+//
+// 5 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//
+// 6 Dichiariamo chi ha vinto.
